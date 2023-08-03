94c4cd4b0fd947d6c471eb4f1bd33f2122a989dc
Johannes Bechberger <johannes.bechberger@sap.com>
2023-08-03 17:28:51 +0200

    Fix VM call in aarch64 _return

--- src/hotspot/cpu/aarch64/templateTable_aarch64.cpp
+++ src/hotspot/cpu/aarch64/templateTable_aarch64.cpp
@@ -2211,7 +2211,7 @@ void TemplateTable::_return(TosState state)
 
     __ push_cont_fastpath(rthread);
     __ set_last_Java_frame(esp, rfp, (address) the_pc, rscratch1);
-    __ super_call_VM_leaf(CAST_FROM_FN_PTR(address, InterpreterRuntime::at_safepoint), rthread);
+    __ call_VM(noreg, CAST_FROM_FN_PTR(address, InterpreterRuntime::at_safepoint), rthread);
     __ reset_last_Java_frame(true);
     __ pop_cont_fastpath(rthread);
     __ pop(state);
