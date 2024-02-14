f8b3c6c3eedec3bb3a1389984fbfa039e41e0d78
Johannes Bechberger <johannes.bechberger@sap.com>
2024-02-14 10:50:52 +0100

    Copy JFR sampler

--- src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataRepository.java
+++ src/jdk.jfr/share/classes/jdk/jfr/internal/MetadataRepository.java
@@ -79,7 +79,7 @@ public final class MetadataRepository {
                 // annotations, such as Period and Threshold.
                 if (pEventType.hasPeriod()) {
                     pEventType.setEventHook(true);
-                    if (!pEventType.isMethodSampling()) {
+                    if (!pEventType.isMethodSampling() || !pEventType.isCPUTimeMethodSampling()) {
                         PeriodicEvents.addJVMEvent(pEventType);
                     }
                 }
